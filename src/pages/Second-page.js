aedfb43da9039e25742c951e4cbebdcb9aa4398a
DmytroShk <dmytro.shkuropatenko@jelvix.com>
2023-02-04 16:21:59 -0600

    added npm d3, and added chart function

--- src/pages/Second-page.js
+++ src/pages/Second-page.js
@@ -1,3 +1,54 @@
+import { useState, useRef, useEffect } from 'react';
+import * as d3 from 'd3';
+
 export const Second = () => {
-  return <div>Second Page</div>
+  const [data] = useState([25, 50, 35, 15, 94, 10]);
+  const svgRef = useRef();
+
+  useEffect(() => {
+    const w = 400;
+    const h = 100;
+    const svg = d3.select(svgRef.current)
+      .attr('width', w)
+      .attr('height', h)
+      .style('background', '#d3d3d3')
+      .style('margin', '50')
+      .style('overflow', 'visible');
+
+    const xScale = d3.scaleLinear()
+      .domain([0, data.length - 1])
+      .range([0, w]);
+    const yScale = d3.scaleLinear()
+      .domain([0, h])
+      .range([h, 0]);
+    const generateScaleLine = d3.line()
+      .x((d, i) => xScale(i))
+      .y(yScale)
+      .curve(d3.curveCardinal);
+
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(data.length)
+      .tickFormat(i => i + 1);
+    const yAxis = d3.axisLeft(yScale)
+      .ticks(5);
+    svg.append('g')
+      .call(xAxis)
+      .attr('transform', `translate(0, ${h})`);
+    svg.append('g')
+      .call(yAxis);
+
+    svg.selectAll('.line')
+      .data([data])
+      .join('path')
+      .attr('d', d => generateScaleLine(d))
+      .attr('fill', 'none')
+      .attr('stroke', 'black');
+
+  }, [data]);
+
+  return (
+    <div>
+      <svg ref={svgRef}></svg>
+    </div>
+  )
 }
